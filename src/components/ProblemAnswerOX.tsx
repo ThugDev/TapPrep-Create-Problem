f254aa30c2578f0ecf838de8b9afb4350cfee069
jinsol <skek298@naver.com>
2025-02-14 16:11:00 +0900

    💡refactor: OX 문제 버튼 위치 수정

--- src/components/ProblemAnswerOX.tsx
+++ src/components/ProblemAnswerOX.tsx
@@ -2,16 +2,16 @@ import { ProblemAnswerOXProps } from './type'
 
 const ProblemAnswerOX = ({ answerOX, setAnswerOX }: ProblemAnswerOXProps) => {
     return (
-        <div className="flex-row gap-4 justify-center items-center">
+        <div className="flex-center flex-col gap-12">
             <button
                 onClick={() => setAnswerOX(true)}
-                className={`px-6 py-3 rounded-lg ${answerOX === true ? 'bg-green-500' : 'bg-gray-300'}`}
+                className={`px-6 py-3 rounded-lg cursor-pointer ${answerOX === true ? 'bg-green-500' : 'bg-gray-300'}`}
             >
                 <p className="text-white text-lg font-bold">O</p>
             </button>
             <button
                 onClick={() => setAnswerOX(false)}
-                className={`px-6 py-3 rounded-lg ${answerOX === false ? 'bg-green-500' : 'bg-gray-300'}`}
+                className={`px-6 py-3 rounded-lg cursor-pointer ${answerOX === false ? 'bg-green-500' : 'bg-gray-300'}`}
             >
                 <p className="text-white text-lg font-bold">X</p>
             </button>
