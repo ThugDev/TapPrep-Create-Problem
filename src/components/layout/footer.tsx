0a56f3c7c05fd023bea9a435d05b01b5d595b566
jinsol <skek298@naver.com>
2025-02-26 16:05:02 +0900

    💡refactor: footer수정 완료

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <div className="w-full h-[50px] fixed">
             <div className="w-full h-full text-xs text-[#073955] flex justify-center items-center ">
-                JinsolKim, ,juyeonOh all rights reserved@2025
+                JinsolKim, sungchulYang, juyeonOh all rights reserved@2025
             </div>
         </div>
     )
