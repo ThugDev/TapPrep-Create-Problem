988fa8059128fe88c488b1e757d5da0ca5ac54c5
jinsol <skek298@naver.com>
2025-03-21 14:54:41 +0900

    🧪Test: 배포 git login test 완료

--- src/auth/page.tsx
+++ src/auth/page.tsx
@@ -10,8 +10,6 @@ const Auth = () => {
         const params = new URLSearchParams(location.search)
         const code = params.get('code')
 
-        console.log('code', code)
-
         if (code) {
             const fetchLoginData = async (code: string) => {
                 const response = await postGitLogin(code)
